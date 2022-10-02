4bd9884c44559349506e8e0f8ec8eb42d64b1c34
Mathieu <huot.mathieuu@gmail.com>
2022-10-02 15:25:48 +0200

    suppression code inutile pour la gestion des pages

--- public/index.php
+++ public/index.php
@@ -2,20 +2,6 @@
 
 require '../vendor/autoload.php';
 
-if(isset($_GET['page']) && $_GET['page'] === '1') {
-     
-     $uri = explode('?', $_SERVER['REQUEST_URI'])[0];
-     $get = $_GET;
-     unset($get['page']);
-     $query = http_build_query($get);
-    if (!empty($query)) {
-        $uri = $uri . '?' . $query;
-    }
-    header('Location: ' . $uri);
-    http_response_code(301);
-    exit();
-}
-
 $frontController = new App\Controller\FrontController();
 $frontController->run();
 
